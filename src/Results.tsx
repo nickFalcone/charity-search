a3c50d2d17aafeff7be30b6d4db2ae2b7e09d8e9
nickFalcone <nfalcone@u.rochester.edu>
2021-02-21 17:01:19 -0500

    Convert Results component to TS

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,35 +1,54 @@
 import React from 'react';
 import Charity from './Charity';
 import Spinner from './Spinner';
-
-const Results = (data) => {
+// TODO: extract to an interface to prevent duplicate type assertion
+const Results = (data: {
+  charities: {
+    ein: string;
+    charityName: string;
+    irsClassification: { nteeClassification: string };
+    mission: string;
+    mailingAddress: { city: string; stateOrProvince: string };
+    charityNavigatorURL: string;
+  }[];
+  loading: boolean;
+}) => {
   return (
     <div className="search-results" aria-live="polite">
       <p className="search-results-count">
         {data.charities.length} result{data.charities.length === 1 ? '' : 's'}
       </p>
       {data.charities.length ? (
-        data.charities.map((charity) => {
-          if (data.loading) {
-            return <Spinner />;
-          } else {
-            return (
-              <Charity
-                key={charity.ein}
-                name={charity.charityName}
-                cause={charity.irsClassification.nteeClassification}
-                mission={charity.mission}
-                city={charity.mailingAddress.city}
-                state={charity.mailingAddress.stateOrProvince}
-                website={charity.charityNavigatorURL}
-                id={charity.ein}
-                path={`/charity/:${charity.charityName
-                  .replace(/\s+/g, '-')
-                  .toLowerCase()}`}
-              />
-            );
+        data.charities.map(
+          (charity: {
+            ein: string;
+            charityName: string;
+            irsClassification: { nteeClassification: string };
+            mission: string;
+            mailingAddress: { city: string; stateOrProvince: string };
+            charityNavigatorURL: string;
+          }) => {
+            if (data.loading) {
+              return <Spinner />;
+            } else {
+              return (
+                <Charity
+                  key={charity.ein}
+                  name={charity.charityName}
+                  cause={charity.irsClassification.nteeClassification}
+                  mission={charity.mission}
+                  city={charity.mailingAddress.city}
+                  state={charity.mailingAddress.stateOrProvince}
+                  website={charity.charityNavigatorURL}
+                  id={charity.ein}
+                  // path={`/charity/:${charity.charityName
+                  //   .replace(/\s+/g, '-')
+                  //   .toLowerCase()}`}
+                />
+              );
+            }
           }
-        })
+        )
       ) : data.loading ? (
         <Spinner />
       ) : (
